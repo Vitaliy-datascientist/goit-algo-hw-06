655ecd4ba3ed36cdf044ae1fdc31c823a11be002
Vitaliy-datascientist <vitaliy.dayneko2410@gmail.com>
2024-07-05 17:00:47 +0300

    first_commit

---
+++ functionality_for_bot.py
@@ -0,0 +1,121 @@
+"""Функціонал для консольного бота помічника(2.0), який розпізнає команди, що вводяться з клавіатури,
+                                    та відповідає відповідно до введеної команди."""
+from collections import UserDict
+
+
+class ValidationError(Exception):
+    def __init__(self):
+        super().__init__('The phone must have 10 symbols.')
+
+
+class Field:
+    """Базовий клас для полів запису."""
+
+    def __init__(self, value: str):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Name(Field):
+    """Клас для зберігання імені контакту."""
+    pass
+
+
+class Phone(Field):
+    """Клас для зберігання номера телефону. Має валідацію формату (10 цифр)."""
+
+    def __init__(self, value: str):
+        if len(value) == 10:
+            super().__init__(value)
+        else:
+            raise ValidationError
+
+
+class Record:
+    """Клас для зберігання інформації про контакт, включаючи ім'я та список телефонів."""
+
+    def __init__(self, name: str, phones: list[str, ...] = None):
+        self.name = Name(name)
+        self.phones = [Phone(phone) for phone in phones] if phones else []
+
+    def add_phone(self, phone: str):
+        """
+        Додавання телефонів.
+
+        :param phone: Бажаний номер телефону.
+        """
+        self.phones.append(Phone(phone))
+
+    def remove_phone(self, phone: str):
+        """
+        Видалення телефонів.
+
+        :param phone: Бажаний номер телефону.
+        """
+        i = [v.value for v in self.phones].index(Phone(phone).value)
+        self.phones.pop(i)
+
+    def edit_phone(self, old_phone: str, new_phone: str):
+        """
+        Редагування телефонів.
+
+        :param old_phone: Старий номер телефону.
+        :param new_phone: Новий номер телефону.
+        """
+        i = [v.value for v in self.phones].index(Phone(old_phone).value)
+        self.phones[i] = Phone(new_phone)
+
+    def find_phone(self, phone: str):
+        """
+        Пошук телефону.
+
+        :param phone: Бажаний номер телефону.
+        :return: Об'єкт номера телефону(Phone) за вказаним номером(phone).
+        """
+        try:
+            i = [v.value for v in self.phones].index(Phone(phone).value)
+            return self.phones[i]
+        except ValueError:
+            return None
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+
+
+class AddressBook(UserDict):
+    """Клас для зберігання та управління записами."""
+
+    def add_record(self, record: Record):
+        """
+        Додавання записів.
+
+        :param record: Бажаний запис.
+        """
+        self.data[record.name.value] = record
+
+    def find(self, name: str):
+        """
+        Пошук записів за іменем.
+
+        :param name: Бажане ім'я для пошуку запису.
+        :return: Об'єкт запису(Records) за вказаним ім'ям(name).
+        """
+        return self.data.get(name)
+
+    def delete(self, name: str):
+        """
+        Видалення записів за іменем.
+
+        :param name: Бажане ім'я для видалення запису.
+        """
+        self.data.pop(name)
+
+    def __str__(self):
+        res = ''
+        n = 1
+        for key in self.data:
+            res += f'{n}. {self.data[key]}\n'
+            n += 1
+        return res
